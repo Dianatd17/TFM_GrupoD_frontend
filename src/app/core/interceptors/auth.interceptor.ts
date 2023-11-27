3c50e96dd799268da1d0ec0b7f5bdf2a5d9b81e3
Christel González Canales <kirisame@gmail.com>
2023-11-27 04:30:04 +0100

    Implementación servicio Usuarios y token (leer descripción)
    * Instalada librería jwt-decode (OJO: instalar con ng install dentro del directorio del proyecto)
    * Form-Login y Form-Register están opetrativos
    * Creado servicio UsuariosService en modules/auth/services con métodos de registro, login, isLogged y getRole
    * Creada guarda de autenticación
    * Creado interceptor para las cabeceras http
    * Para las pruebas del front, al hacer login en /auth/login se llama a una api mock y se crea un token cualquiera. Si se necesitara un rol que no sea el que tiene (admin), hay un método en el servicio de Usuarios (getRole) que se podría editar para pruebas para que devuelva el rol que necesitemos

---
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    console.log('Pasa por el interceptor');
+    const token = localStorage.getItem('auth_token');
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          authorization: token
+        }
+      });
+    }
+    return next.handle(request);
+  }
+}
--- src/app/modules/auth/components/form-login/form-login.component.ts
+++ src/app/modules/auth/components/form-login/form-login.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UsuariosService } from '../../services/usuarios.service';
 
 @Component({
   selector: 'app-form-login',
@@ -12,7 +13,7 @@ export class FormLoginComponent {
   formLogin: FormGroup;
   errorMessage: string = "";
   router = inject(Router);
-  //usuariosService = inject(UsuariosService);
+  usuariosService = inject(UsuariosService);
 
   constructor() {
     this.formLogin = new FormGroup({
@@ -27,24 +28,27 @@ export class FormLoginComponent {
     }, []);
   }
 
+  //TODO: si hay token debería de reenviar a home si es cliente, área de usuario si es logopeda, descomentar cuando esté bien
+  /*ngOnInit(): void {
+    if (localStorage.getItem('auth_token')) this.router.navigate(['/home']);
+  }*/
+
   checkControl(formcontrolName: string, validator: string): boolean | undefined {
     return this.formLogin.get(formcontrolName)?.hasError(validator) && this.formLogin.get(formcontrolName)?.touched;
   }
 
 
   async onSubmit() {
-    //TODO: descomentar cuando esté la función de login en el servicio de usuarios
-    /*const response = await this.usuariosService.login(this.formLogin.value);
+    const response = await this.usuariosService.login(this.formLogin.value);
     if (response.fatal) {
       // Error en el login
       this.errorMessage = response.fatal;
     } else {
       // Login correcto
       localStorage.setItem('auth_token', response.token);
-      // Navego a la ruta de productos
+      //TODO: cambiar para que reenvie al área de usuario si es logopeda en vez de home
       this.router.navigate(['/home']);
     }
-    console.log(response.token);*/
   }
 
 }
