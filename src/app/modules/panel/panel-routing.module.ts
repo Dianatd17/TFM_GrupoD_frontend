d6532a6b9de6c6fa243dce86b372c0c9fd9efdb6
bielmc11 <bielmarticr@gmail.com>
2023-11-30 22:25:22 +0100

    20/11/23 ruta basica panel finalizada

--- src/app/modules/panel/panel-routing.module.ts
+++ src/app/modules/panel/panel-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PanelComponent } from './panel/panel.component';
+import { NotificacionesComponent } from './components/notificaciones/notificaciones.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { LogopedasComponent } from './components/logopedas/logopedas.component';
 
 const routes: Routes = [
-  {path: '',  component:PanelComponent} //Completar aqui
+  {
+    path: '',  component:PanelComponent,
+    children: [
+      {path: 'notificaciones', component: NotificacionesComponent},
+      {path: 'clientes', component: ClientesComponent},
+      {path: 'logopedas', component: LogopedasComponent}
+    ]
+
+  },
+   
   
 ];
 
