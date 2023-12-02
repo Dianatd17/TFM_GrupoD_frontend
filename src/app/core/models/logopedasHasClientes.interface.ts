89b5ef3cbf1217812d35efdf3a9b874b0dc20e14
Diana Taborda Delgado <dianafer_taborda@hotmail.com>
2023-12-02 19:09:11 +0100

    Creación de la interface logopedaHasClientes

--- src/app/core/models/logopedasHasClientes.interface.ts
+++ src/app/core/models/logopedasHasClientes.interface.ts
@@ -3,9 +3,9 @@ export interface IlogopedaHasClientes {
     id: number
     logopeda_id: number
     cliente_id: number
-    comentarios: string
-    puntuacion: number
+    comentarios?: string
+    puntuacion?: number
     fecha_inicio: string
-    fecha_fin: string
-    status: string
+    fecha_fin?: string
+    status?: string
 }
--- src/app/modules/logopedas/components/logopedas-card/logopedas-card.component.ts
+++ src/app/modules/logopedas/components/logopedas-card/logopedas-card.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, inject } from '@angular/core';
 import { ILogopeda } from 'src/app/core/models/logopeda.interface';
 import { UsuariosService } from 'src/app/modules/auth/services/usuarios.service';
 import { LogopedasService } from '../../services/logopedas.service';
+import { IlogopedaHasClientes } from 'src/app/core/models/logopedasHasClientes.interface';
+import { DatePipe } from '@angular/common';
 
 
 @Component({
@@ -10,8 +12,8 @@ import { LogopedasService } from '../../services/logopedas.service';
   styleUrls: ['./logopedas-card.component.css']
 })
 export class LogopedasCardComponent {
-
-
+  clienteContactar: IlogopedaHasClientes = { id: 0, logopeda_id: 0, cliente_id: 0, comentarios: "", puntuacion: 0, fecha_inicio: "", status: "pendiente" };
+  pipe = new DatePipe('es-US');
 
   logopedasServices = inject(LogopedasService)
   usuariosService = inject(UsuariosService);
@@ -21,12 +23,24 @@ export class LogopedasCardComponent {
   };
 
 
-  contactarLogopeda(idLogopeda: number) {
+  async contactarLogopeda(idLogopeda: number) {
+    debugger;
     const idCliente: number = this.usuariosService.getIdUsuario()
-    if (idCliente != 0) {
-      //const response = await this.logopedasServices.createContactarLogopedaHasCliente(edad);
-      //  this.arrUsers = response;
+    console.log(idCliente)
+    if (idCliente !== 0) {
 
+      //   this.clienteContactar.logopeda_id = idLogopeda
+      //  this.clienteContactar.cliente_id = idCliente
+      //   this.clienteContactar.comentarios = ""
+      // this.clienteContactar.puntuacion = 0
+      //this.clienteContactar.fecha_inicio = this.pipe.transform(Date.now(), 'yyyy-MM-dd')!;
+
+      //console.log(this.clienteContactar)
+      // const response = await this.logopedasServices.createContactarLogopedaHasCliente(this.clienteContactar);
+      // console.log(response)
+      //if (response.id) {
+      // console.log('Se ha enviado la notificación a:' + this.miLogopeda.nombre + ' ' + this.miLogopeda.apellidos)
+      // }
     }
   }
 }
