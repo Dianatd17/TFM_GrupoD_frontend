4cb743adaba6958bb13ec8d6362f369a7f2f6c29
bielmc11 <bielmarticr@gmail.com>
2023-12-06 20:31:17 +0100

    mislogopedas casi acabado

--- src/app/modules/panel/components/logopedas/logopedas.component.ts
+++ src/app/modules/panel/components/logopedas/logopedas.component.ts
@@ -9,15 +9,32 @@ import { logopedas } from '../../interfaces/panel.interfaces';
 })
 export class LogopedasComponent {
   arrLogopedas: logopedas[] | any[] = []
+  localidad: string[] = []
   panelService = inject(PanelService)
 
   async ngOnInit(){
     try{
-      this.arrLogopedas = await this.panelService.getLgopedasByClientes();
-      console.log(this.arrLogopedas)
+      this.arrLogopedas = (await this.panelService.getLgopedasByClientes()).filter(log => log.estado_u === 1);
+
+      this.localidad = this.arrLogopedas.map(log => log.localidad)
+    
     } catch(err){
       console.log(err)
     }
   }
+
+  colorStatus(status:string){
+    switch(status){
+      case 'aceptado':
+        return '#198754'
+      case 'pendiente':
+        return '#FFC107'
+      default:
+         '#60BBDD'
+    }
+    return ''
+
+  }
+
 
 }
