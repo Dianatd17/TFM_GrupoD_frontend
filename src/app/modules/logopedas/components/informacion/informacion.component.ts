cbaa3b16d43840ffe7863e0919943af2b4f88554
Christel González Canales <kirisame@gmail.com>
2023-12-11 17:51:06 +0100

    Ajustes de interfaz de usuario en logopeda-card e información perfil

--- src/app/modules/logopedas/components/informacion/informacion.component.ts
+++ src/app/modules/logopedas/components/informacion/informacion.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ILogopeda } from 'src/app/core/models/logopeda.interface';
 import { UsuariosService } from 'src/app/modules/auth/services/usuarios.service';
 import { IlogopedaHasClientes } from '../../../../core/models/logopedasHasClientes.interface';
+import { DatePipe } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-informacion',
@@ -14,14 +16,17 @@ export class InformacionComponent {
   isLog: boolean = false;
   idLogopeda: number = 0;
   rol: string = "";
+  pipe = new DatePipe('en-ES');
   miLogopeda: ILogopeda = {
     id: 0, nombre: "", apellidos: "", direccion: "", email: "", telefono: "", precio: 0, longitud: 0, latitud: 0, descripcion: "", experiencia: 0, imagen: "", infancia_o_adulto: ""
   };
+  clienteContactar: IlogopedaHasClientes = { id: 0, logopeda_id: 0, cliente_id: 0, comentarios: "", puntuacion: 0, fecha_inicio: "", status: "pendiente" };
 
   router = inject(Router);
   activeRoute = inject(ActivatedRoute);
   logopedasServices = inject(LogopedasService);
   usuarioService = inject(UsuariosService);
+  private toastrService = inject(ToastrService);
   conexion: IlogopedaHasClientes[] = [];
 
 
@@ -64,8 +69,26 @@ export class InformacionComponent {
     }
   }
 
-  async getConexion() {
-    
-  }
+  async contactarLogopeda(idLogopeda: number) {
 
+    const idCliente: number = this.usuarioService.getIdUsuario()
+
+    if (idCliente !== 0) {
+      let currentDate = new Date();
+      this.clienteContactar.logopeda_id = this.miLogopeda.id
+      this.clienteContactar.cliente_id = idCliente
+      this.clienteContactar.comentarios = ""
+      this.clienteContactar.puntuacion = 0
+      this.clienteContactar.fecha_inicio = this.pipe.transform(currentDate, 'yyyy-MM-dd')!;
+
+      const response = await this.logopedasServices.createContactarLogopedaHasCliente(this.clienteContactar);
+
+      if (response) {
+        this.toastrService.success('Notificación enviada', 'Contactar logopeda')
+      } else {
+        this.toastrService.error('No se ha podido enviar la notificación de contacto')
+      }
+    }
+
+  }
 }
