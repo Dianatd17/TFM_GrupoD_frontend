62968d4be302a6b4a68d9edca6ed9730e64c65fb
Christel González Canales <kirisame@gmail.com>
2023-12-11 14:06:08 +0100

    Añadido envío de notificaciones por email a los administradores

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -7,7 +7,6 @@ export const authGuard: CanActivateChildFn = (childRoute, state) => {
   const router = inject(Router);
 
   if (localStorage.getItem('auth_token')) {
-    // TODO: falta comprobar si el token es correcto en el server
     const token = localStorage.getItem('auth_token');
     const tokenDecode = jwtDecode(token!);
     return true;
